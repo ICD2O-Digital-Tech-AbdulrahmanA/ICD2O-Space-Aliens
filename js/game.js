bc3b9d3f6a08944dc9f5cc34c442de8312dcd4ba
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-05-20 14:56:47 +0000

    rst

---
+++ js/game.js
@@ -0,0 +1,34 @@
+// Game Scene
+
+import SplashScene from './splashScene.js'
+
+// our game scene
+const SplashScene = new SplashScene()
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    hegiht: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    // Set Background Color;
+    backgroundColor: 0x5f6e7a,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        // We Place It in the middle of the page
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
+
+const game = new Phaser.Game(config)
+
+// load scenes
+// Note: re,e,ner amu "key" is global and can not be reused.
+game.scene.add('splashScene', splashScene)
+
+// start title
+game.scene.start('splashScene')
---
+++ js/splashScene.js
@@ -0,0 +1,21 @@
+class SplashScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'splashScene' })
+    }
+
+    init(data) {
+        this.cameras.main.setBackgroundColor('#ffffff')
+    }
+
+    preload() {
+        console.log('Splash Scene')
+    }
+
+    create(data) {       
+    }
+
+    update(time.delta) {
+    }
+}
+
+export default SplashScene
