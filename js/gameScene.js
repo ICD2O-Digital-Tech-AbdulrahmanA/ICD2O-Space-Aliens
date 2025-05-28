b19d879f29bdc8135752ea4f846f8ea41e87258b
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-05-28 14:31:11 +0000

    011 redo

--- js/gameScene.js
+++ js/gameScene.js
@@ -93,7 +93,7 @@ class GameScene extends Phaser.Scene {
         }
 
         this.missileGroup.children.each(function (item) {
-            item.y = item.y - 45
+            item.y = item.y - 15
             if (item.y < 0) {
                 item.destroy()
             }
